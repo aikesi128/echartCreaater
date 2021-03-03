978b78d6ca4282d88faa9d193c740b9b30c19244
zhujunwu <aikesi128@163.com>
2021-03-03 14:00:21 +0800

    Initial Commit

---
+++ test_mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  test_mac
+//
+//  Created by zhujunwu on 2021/3/3.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ test_mac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test_mac
+//
+//  Created by zhujunwu on 2021/3/3.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
